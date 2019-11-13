bc9ec254454aeda2298e72e9344cc9439c62a1bf
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-13 23:05:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,1 @@
 # Your code goes here!
-class Anagram
-
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
-  end
-
-  def match(words_array)
-    word_array.select do |word|
-    (@word.split("").sort) ==
-    (word.split("").sort)
-  end
-  
