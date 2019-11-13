ebf5ebaa6dddd15f5388c749b42b01d5d401da3a
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-13 22:59:32 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,4 +4,12 @@ class Anagram
   def initialize(word)
     @word =  word
   end
-end
+  end
+  def match(array_of_words)
+    array_of_words.select do |element|
+    (@word.split("").sort) == 
+    (element.split("").sort)
+  end
+
+  listen = anagrams.new("listen")
+  listen.match(%(enlist, google. inlets, banana)
