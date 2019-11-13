f6bffbf7e46eb29ed5b27e9acfe6ab5480ed7ecc
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-13 23:02:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,14 @@
 # Your code goes here!
 class Anagram
+  
   attr_accessor :word
-
+  
   def initialize(word)
-    @word =  word
+    @word = word
   end
-  end
+  
   def match(array_of_words)
-    array_of_words.select do |element|
+  array_of_words.select do |element|
     (@word.split("").sort) ==
     (element.split("").sort)
   end
